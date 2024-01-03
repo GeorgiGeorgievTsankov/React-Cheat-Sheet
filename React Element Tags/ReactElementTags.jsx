6264ff99de80d26de6ae4b4df980bcffb6b9cee8
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-03 18:21:43 +0200

    Add files via upload

---
+++ React Element Tags/ReactElementTags.jsx
@@ -0,0 +1,14 @@
+// React elements look just like HTML, in fact they render the same equivalent HTML elements.
+
+<h1>My Header</h1>
+<button>My Button</button>
+<ul>
+	<li>list item 1</li>
+	<li>list item 2</li>
+	<li>list item 3</li>
+</ul> 
+
+
+// Single tag elements like the img element and the br element must be closed like so
+<img src="my-image-source.jpeg" />
+<br />
