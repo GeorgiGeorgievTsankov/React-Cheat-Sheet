e81865ad39398b00560a9192b8fbd1eafd9b39db
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-05 14:24:32 +0200

    Create React Element Attributes.jsx

---
+++ React Element Tags/React Element Attributes.jsx
@@ -0,0 +1,6 @@
+// React elements have different attributes compares to their HTML counterparts. Since JSX is still javascript, we use camelcase.
+// Also, class is a protected keyword in javascript (creating classes) so the HTML class attribute in JSX is className.
+
+<div className="my-container">
+	<img className="my-container-image" src="my-image.jpeg"/>
+</div>
