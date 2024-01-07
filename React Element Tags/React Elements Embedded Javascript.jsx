e8a8be6b4aa3abd7e7e2894ff9092fb4e6c13559
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-07 17:41:05 +0200

    Create React Elements Embedded Javascript.jsx

---
+++ React Element Tags/React Elements Embedded Javascript.jsx
@@ -0,0 +1,5 @@
+// The power of JSX is that it’s javascript and HTML. This means you can write javascript to render different attributes directly in your javascript using curly braces {} .
+
+const divClass = "my-div-class"
+
+<div className={divClass}></div>
