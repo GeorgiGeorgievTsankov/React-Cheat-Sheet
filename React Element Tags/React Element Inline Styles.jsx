8f41d3ecba1a83c6ad5b7ed4711acf673bb34fc0
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-01-09 18:14:29 +0200

    Create React Element Inline Styles.jsx

---
+++ React Element Tags/React Element Inline Styles.jsx
@@ -0,0 +1,9 @@
+// React elements just like HTML elements can use the style attribute, but you pass them as javascript objects instead of double quote strings.
+
+// In HTML
+
+<h1 style="color:blue;text-align:center">This is a header</h1>
+  
+// In JSX
+
+<h1 style={{ color: 'blue', textAlign: 'center' }}>This is a header</h1>
